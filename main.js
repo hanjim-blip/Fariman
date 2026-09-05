20ea54bbb1ca851c30ed2502373d821fab0aff37
hanjim-blip <videyhanjim@gmail.com>
2026-09-06 00:41:20 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ shareBtn.onclick = () => {
 };
 /* UNDANGAN */
 function joinWhatsAppGroup() {
-    window.open("https://vt.tokopedia.com/t/ZS9BVemBj33ft-g7npw/");
+    window.open("https://s.shopee.co.id/3Vk0N7YnVy");
 }
 
 function openFacebookPage() {
